9f703fb40fe2f746704caee8ddc94f77ecc8dd76
Mena May Joel Enrique <16470287@itcampeche.edu.mx>
2020-03-24 15:30:23 -0600

    Quinto commit: Se agregó el componente Dashboard.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 //Importa dependencias
-import { StocksService, StockInterface } from './services/stocks.service';
+//import { StocksService, StockInterface } from './services/stocks.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app'; // si falla algo vuelve a poner:   stocks
+
+export class AppComponent {}
+/*  title = 'app'; // si falla algo vuelve a poner:   stocks
   
   //Declara una propiedad como un arreglo de acciones
   stocks: Array<StockInterface>;
@@ -19,4 +20,4 @@ export class AppComponent {
       this.stocks = stocks;
     });
   }
-}
+}*/
